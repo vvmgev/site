7786e5ed17fddcbf59c574a52bcbf3975a001fbe
Baghdasaryan <vvmgev@gamil.com>
2024-11-24 14:45:10 +0400

    asa

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("test!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
